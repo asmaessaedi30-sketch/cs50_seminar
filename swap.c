9c845a0874bb70fce9f76d2fd96ab18f61e10549
Asma <Asmaessaedi30@gmail.com>
2026-06-29 13:28:10 -0400

    adding a swap function

--- swap.c
+++ swap.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main(void)
 {
     int x = 51;
     int y = 10;
 
-    printf("x is %d\n", x)
-    printf("y is %d\n", y)
+    printf("x is %d\n", x);
+    printf("y is %d\n", y);
 
 
 
-int temp = x;
-x = y;
-y = temp;
+swap(&x, &y);
 
-printf("x is %d\n", x)
-printf("y is %d\n", y)
+printf("x is %d\n", x);
+printf("y is %d\n", y);
 
 return 0;
 
